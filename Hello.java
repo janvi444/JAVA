4cdacca8471440da3db5adc6c096b2c755da6b8b
ydv-kanchan <ydv.kanchan24@gmail.com>
2024-07-16 10:48:29 +0530

    hello & next line

---
+++ Hello.java
@@ -0,0 +1,17 @@
+//this line defines a public class named Hello 
+//in java all code must reside within a cclass
+
+public class Hello {
+    /*this line defines the main method which is the entry point of any java application
+    public means the method can be called from outside the class
+    static means that the method belongs to the class rather than an instance of class
+    void indicates that the method does not return a vlaue.
+    String[] argsis an array of string arguments passed to the program*/
+    public static void main(String[] args) {
+        
+        //System.out.printIn is standard method to output text to the console
+        System.out.print("hello"); 
+        System.out.println("hiii"); //println is endl of c++
+        System.out.print("kanchan");
+    }
+}
